0d0ada49d9892eee755cd9ca8bfd0e9c454e7aeb
BrandonSoldado <112739941+BrandonSoldado@users.noreply.github.com>
2024-05-18 13:12:50 -0400

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, session
+from flask import Flask, request, session, make_response
 from twilio.twiml.messaging_response import MessagingResponse
 from twilio.rest import Client
 import psycopg2
@@ -24,23 +24,31 @@ def obtener_nombre_usuario(telefono):
     conn.close()
     return nombre[0] if nombre else ""
 
+
 @app.route("/webhook", methods=["POST"])
 def webhook():
-    message_body = request.form.get("Body")  
+    message_body = request.form.get("Body")
     from_number = request.form.get("From")
-    to_number = request.form.get("To") 
+    to_number = request.form.get("To")
     nombre = obtener_nombre_usuario(from_number)
-    if(nombre!=""):
-        respuesta = "¡Hola! "+nombre + " Soy el asistente de servicios de elevadores. Estoy aquí para ayudarte contus consultas y solicitudes relacionadas con los elevadores. Puedo ayudarte a solicitar servicios de mantenimiento,monitorear el estado de los elevadores en tiempo real, programar horarios de operación y mucho más. ¿En qué puedo ayudarte hoy?"
-    else:
-        respuesta = "usuario no existe!"
     
-    print(f"El mensaje fue enviado a: {from_number}")  
+    # Verificar si el mensaje es "confirmar"
+    if message_body.lower() == "confirm":
+        print(f"Confirmación recibida de: {from_number}")
+        return make_response("", 204)  # No Content
+
+    if nombre:
+        respuesta = f"¡Hola! {nombre}, soy el asistente de servicios de elevadores. Estoy aquí para ayudarte con tus consultas y solicitudes relacionadas con los elevadores. Puedo ayudarte a solicitar servicios de mantenimiento, monitorear el estado de los elevadores en tiempo real, programar horarios de operación y mucho más. ¿En qué puedo ayudarte hoy?"
+    else:
+        respuesta = "Usuario no existe!"
+
+    print(f"El mensaje fue enviado a: {from_number}")
     print(respuesta)
+    print(message_body)
+    
     resp = MessagingResponse()
     resp.message(respuesta)
     return str(resp)
-
 
   
 if __name__=='__main__':
